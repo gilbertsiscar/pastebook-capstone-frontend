7d7e982806f3eb4ca9c231b8b21f3d1e43a0b7d5
gilbertsiscar <gilbert.siscar@pointwest.com.ph>
2022-03-08 09:40:00 +0800

    Refactored login-form/components import paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from '@pages/login/login.component';
+import { LoginFormComponent } from '@components/login-form/login-form.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { LoginFormComponent } from './components/login-form/login-form.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent }, // http://localhost:4200/,
