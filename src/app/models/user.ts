c70dc11e8940a5a95a40ccbf79bfd7d81a4a800e
Maria Donessa Cayas <donessa.cayas@pointwest.com.ph>
2022-03-09 14:16:50 +0800

    Edit user model, user service, edit-profile & edit-security comp

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,1 +1,18 @@
-export class User {}
+export class User {
+    
+    constructor(
+        public id?: number,
+        public firstName?: string,
+        public lastName?: string,
+        public email?: string,
+        public password?: string,
+        public birthday?: string,
+	    public gender?: string,
+	    public mobileNumber?: string,
+	    public isOnline?: false,
+	    public datetimeCreated?: string,
+	    public profilePic?: string,
+	    public aboutMe?: string,
+	    public profileUrl?: string
+    ) {}
+}
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private baseUrl = `${environment.apiUrl}/users`;
+
+  //not sure kung tama paglagay ng path based sa backend
+  private baseUrl: string = environment.apiUrl + '/users/details';
+  private httpHeaders: HttpHeaders = new HttpHeaders({
+    'Authorization': `Bearer' ${this.sessionService.getToken()}`
+  })
 
   constructor(private http: HttpClient) {}
 
@@ -16,4 +22,15 @@ export class UserService {
     if (debug) return of({ email, password });
     return this.http.post(`${this.baseUrl}/login`, { email, password });
   }
+
+  getUser(id: number): Observable<User[]> {
+    return this.http.get<User[]>(`${this.baseUrl}/${id}`);
+  }
+
+  updatePersonalInfo(user: User): Observable<Object> {
+    return this.http.put(this.baseUrl + `/${user.id}`, user, {headers: this.httpHeaders});
+  }
+
+  
+
 }
