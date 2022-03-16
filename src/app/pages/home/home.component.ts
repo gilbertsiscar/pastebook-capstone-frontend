078baff0464c84c58ec5ba4cf870e4c8be4aaaaf
gilbertsiscar <gilbert.siscar@pointwest.com.ph>
2022-03-16 23:33:45 +0800

    Implement pagination

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Post } from 'src/app/models/post';
 import { PostService } from 'src/app/services/post.service';
 
@@ -7,29 +15,82 @@ import { PostService } from 'src/app/services/post.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  posts: Post[];
+export class HomeComponent implements OnInit, OnDestroy {
+  @ViewChildren('theLastList', { read: ElementRef })
+  theLastList: QueryList<ElementRef>;
+
+  postSubscription: Subscription;
+  posts: Post[] = [];
+
+  totalPages: number;
+  currentPage: number = 0;
+
+  observer: IntersectionObserver;
 
   constructor(private postService: PostService) {}
 
   ngOnInit(): void {
-    this.postService.getPosts().subscribe((posts) => {
-      if (posts) {
-        this.posts = posts.sort(
-          (a, b) =>
-            <any>new Date(b.datetimeCreated) - <any>new Date(a.datetimeCreated)
-        );
+    this.getPosts();
+    this.intersectionObserver();
+  }
+
+  ngAfterViewInit() {
+    this.theLastList.changes.subscribe((data) => {
+      if (data.last) {
+        this.observer.observe(data.last.nativeElement);
       }
     });
   }
 
-  onDelete(id: string) {
+  ngOnDestroy(): void {
+    this.postSubscription.unsubscribe();
+  }
+
+  intersectionObserver() {
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    };
+
+    this.observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        if (this.currentPage < this.totalPages && this.posts.length === 10) {
+          this.currentPage++;
+          this.adpendPost();
+        }
+      }
+    }, options);
+  }
+
+  adpendPost() {
+    this.postService
+      .getPostsPagination(this.currentPage)
+      .subscribe((res) =>
+        res.content.forEach((post: Post) => this.posts.push(post))
+      );
+  }
+
+  getPosts() {
+    this.postSubscription = this.postService
+      .getPostsPagination(this.currentPage)
+      .subscribe((res) => {
+        this.totalPages = res.totalPages;
+        this.posts = res.content;
+        // res.content.forEach((post: Post) => this.posts.push(post));
+        console.log(this.posts);
+      });
+  }
+
+  onDelete(id: string, index: number) {
+    console.log(index);
+    this.posts.splice(index, 1);
     this.postService.deletePost(id).subscribe(() => {
-      this.ngOnInit();
+      this.getPosts();
     });
   }
 
   onRefresh() {
-    this.ngOnInit();
+    this.getPosts();
   }
 }
